6b823f9965b3ceb72b3b4162539bbfe999f94c40
Nicolas C. <github@liliwol.fr>
2024-11-20 22:00:52 +0100

    Env file Postman Collection

--- BACKEND/config/database.js
+++ BACKEND/config/database.js
@@ -2,14 +2,14 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 
 // Load dotenv file
-const env = dotenv.config( { path: '../.env.docker' } );
+const env = dotenv.config( { path: '.env.docker' } );
 console.log(env);
 
 // create the connection to database
 const db = mysql.createConnection(
     {
-        host:           env.parsed.DB_HOST_FOR_NODE,
-        port:           env.parsed.DB_PORT_FOR_NODE,
+        host:           env.parsed.DB_HOST,
+        port:           env.parsed.DB_PORT,
         user:           env.parsed.DB_USER,
         password:       env.parsed.DB_PASSWORD,
         database:       env.parsed.DB_NAME
